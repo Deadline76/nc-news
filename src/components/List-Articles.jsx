d7912e525589ca1313cfaa81eb6e5c8540be7164
Deadline76 <dajc76@aol.com>
2023-09-05 12:33:43 +0100

    adds loading state

--- src/components/List-Articles.jsx
+++ src/components/List-Articles.jsx
@@ -5,20 +5,26 @@ import { useParams } from "react-router-dom"
 
 export const ListArticles = (props) => {
     const [articles, setArticles] = useState([])
+    const [isloading, setIsLoading] = useState(true)
+
     
 
     useEffect(() => {
         if (!props.topic) {
             getAllArticles().then((data) => {
                 setArticles(data)
+                setIsLoading(false)
             })
         } else {
-        getArticlesByTopic(props.topic).then((data) => {
-            setArticles(data)
+            getArticlesByTopic(props.topic).then((data) => {
+                setArticles(data)
+                setIsLoading(false)
         })
     }
     }, [props.topic])
 
+    if (isloading) return <h2 className="loading">Loading...</h2>
+ 
 
     return (
         <div>
@@ -27,8 +33,8 @@ export const ListArticles = (props) => {
             return (
                 <section key={article_id} className="article-section">
                     <h2 className="article-title">{title}</h2>
-                    <p className="article-topic">{topic}</p>
-                    <p className="article-votes">Votes: {votes}</p>
+                    <h4 className="article-topic">{topic}</h4>
+                    <span className="article-votes">Votes: {votes}</span>
                 </section>
             )
         }))}
--- src/components/List-Topics.jsx
+++ src/components/List-Topics.jsx
@@ -5,13 +5,17 @@ import { Link } from 'react-router-dom'
 
 export const ListTopics = () => {
     const [allTopics, setAllTopics] = useState([])
+    const [isloading, setIsLoading] = useState(true)
 
     useEffect(() => {
         getTopics().then((topics) => {
             setAllTopics(topics)
+            setIsLoading(false)
             
         })
     }, [])
+
+    if (isloading) return <h2 className="loading">Loading...</h2>
 
     return (
         <div className="category-container">
